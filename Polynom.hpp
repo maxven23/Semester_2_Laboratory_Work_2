4cba772726a7ac7a0ed637d6a5d8c9a12fd38389
Max <56136235+maxven23@users.noreply.github.com>
2020-05-25 02:39:48 +0300

    // Update #5

--- Polynom.hpp
+++ Polynom.hpp
@@ -31,7 +31,7 @@ public:
 		return this->coefficients->GetSize();
 	};
 
-	Polynom<T>& operator + (Polynom& polynom) {
+	Polynom<T>& operator + (Polynom<T>& polynom) {
 		Polynom<T>* newpoly;
 		if (this->GetDegree() >= polynom.GetDegree()) {
 			int tempDeg = this->degree;
@@ -57,8 +57,90 @@ public:
 		this->coefficients->Set(i, item);
 	};
 
-	T Get(int i) {
-		return this->coefficients->Get(i);
-	}
+	T Get(int wdegree) {
+		return this->coefficients->Get(wdegree);
+	};
+
+	Polynom<T>& operator - (Polynom<T>&  polynom) {
+		Polynom<T>* newpoly;
+		if (this->GetDegree() >= polynom.GetDegree()) {
+			int tempDeg = this->degree;
+			newpoly = new Polynom<T>(tempDeg);
+			for (int i = 0; i < polynom.GetDegree(); i++) {
+				newpoly->coefficients->Set(i, this->coefficients->Get(i) - polynom.coefficients->Get(i));
+			}
+		}
+		else {
+			int tempDeg = this->degree;
+			newpoly = new Polynom<T>(polynom.GetDegree());
+			for (int i = tempDeg; i < polynom.GetDegree(); i++) {
+				newpoly->coefficients->Set(i, polynom.coefficients->Get(i));
+			}
+			for (int i = 0; i < tempDeg; i++) {
+				newpoly->coefficients->Set(i, this->coefficients->Get(i) - polynom.coefficients->Get(i));
+			}
+		}
+		return *newpoly;
+	};
+
+	Polynom<T>& operator * (T multiplier) {
+		Polynom<T>* newpoly;
+		newpoly = new Polynom<T>(this->degree);
+		for (int i = 0; i < this->degree; i++) {
+			newpoly->coefficients->Set(i, this->coefficients->Get(i) * multiplier);
+		}
+		return *newpoly;
+	};
+
+	Polynom<T>& operator / (T divider) {
+		if (divider == 0)
+			throw std::exception("INVALID INPUT: Division by ZERO");
+		Polynom<T>* newpoly;
+		newpoly = new Polynom<T>(this->GetDegree());
+		for (int i = 0; i < this->GetDegree(); i++) {
+			newpoly->coefficients->Set(i, this->coefficients->Get(i) / divider);
+		}
+		return *newpoly;
+	};
+
+	void Calculate(T x) {
+		T result = { 0 };
+		for (int i = 0; i < this->GetDegree(); i++) {
+			result = result + (this->coefficients->Get(i) * (pow(x, i)));
+		}
+		std::cout << result << std::endl;
+	};
+
+	void Print() {
+		for (int i = 0; i < this->GetDegree() - 1; i++) {
+			std::cout << this->coefficients->Get(i) << " * x^" << i << " + ";
+		}
+		std::cout << this->coefficients->Get(this->GetDegree() - 1) << " * x^" << this->GetDegree() - 1 << std::endl;
+	};
+
+	
+	Polynom<T>& operator * (Polynom<T>& poly) {
+		Polynom<T>* newpoly;
+		newpoly = new Polynom<T>(this->GetDegree() + poly.GetDegree());
+		T* newcoef = new T[this->GetDegree() + poly.GetDegree()];
+		for (int i = 0; i < this->GetDegree() + poly.GetDegree(); ++i) {
+			newcoef[i] = { 0 };
+		}
+
+		for (int i = 0; i < this->GetDegree(); ++i) {
+			for (int j = 0; j < poly.GetDegree(); ++j) {
+				newcoef[i + j] += this->coefficients->Get(i) * poly.coefficients->Get(j);
+				newpoly->coefficients->Set(i + j, newcoef[i + j]);
+			}
+		}
+		return *newpoly;
+	};
+
+	void Compose(Polynom<T>& poly) {
+		Polynom<T>* newpoly;
+		newpoly = new Polynom<T>(this->GetDegree() * poly.GetDegree());
+
+	};
+	
 
 };
--- main.cpp
+++ main.cpp
@@ -11,21 +11,11 @@ using namespace std;
 
 
 int main(void) {
-	int deg = 5;
-	int deg1 = 5;
-	Polynom<int>* test;
-	test = new Polynom<int>(deg);
-	Polynom<int>* test1;
-	test1 = new Polynom<int>(deg1);
-	Polynom<int>* test2 = new Polynom<int>(1);
-	
+	const int INTEGER = 1;
+	const int FLOAT = 2;
+	const int COMPLEX = 3;
 
-	test->Set(2, 123);
-	test1->Set(2, 321);
 
-	*test2 = *test + *test1;
-
-	cout << test2->Get(2) << " Hehe " << test->Get(2);
 
 	return 0;
 }
